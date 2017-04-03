867d4d21f6ff52a1b75634ee3b5ce28bdded20d4
Aman Agrawala <amanagrawala@gmail.com>
2017-04-02 22:55:07 -0400

    feat: Rearranged name and title. Added CSS designs.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,6 +1,16 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
- $("#main").append(["Aman"]);
- var awesomeThoughts  = "I am Aman and I am AWESOME!"
- console.log(awesomeThoughts);
+ //$("#main").append(["Aman"]);
+ 
+ //var awesomeThoughts  = "I am Aman and I am AWESOME!"
+ //console.log(awesomeThoughts);
+
+//var funThoughts = awesomeThoughts.replace("AWESOME","FUN");
+//$("#main").append(funThoughts);
+
+var formattedName = HTMLheaderName.replace("%data%","Aman Agrawala");
+var formattedRole = HTMLheaderRole.replace("%data%","Software Engineer");
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
